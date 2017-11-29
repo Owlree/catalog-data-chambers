0706e3809764400cb9f6a4a3653c279cee81628b
Robert Badea <owlree128@gmail.com>
2017-11-30 01:50:46 +0200

    Added example file

---
+++ serverless/cdep_initiatives.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape(event: dict, context):
+    response = requests.get("http://www.cdep.ro/pls/proiecte/upl_pck2015.lista?cam=2&anp=2017")
+    bs = BeautifulSoup(response)
+    return "Hello, World!"
