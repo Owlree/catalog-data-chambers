2e59488ba35f1bc2102302ffa5deae7a72dc768a
Robert Badea <owlree128@gmail.com>
2017-11-30 16:42:17 +0200

    Added boto

--- serverless/cdep_initiatives.py
+++ serverless/cdep_initiatives.py
@@ -1,5 +1,12 @@
+import time
+import urllib
+
 import requests
+import boto3
 from bs4 import BeautifulSoup
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('catpol_cdep_initiatives')
 
 
 def scrape(event: dict, context):
@@ -7,28 +14,26 @@ def scrape(event: dict, context):
     initiatives = []
     for year in event["years"]:
         url = "http://www.cdep.ro/pls/proiecte/upl_pck2015.lista?cam=2&anp={year}".format(year=year)
+        now = int(time.time())
         response = requests.get(url)
         bs = BeautifulSoup(response.content)
         table_body = bs.select_one("#olddiv > div.grup-parlamentar-list.grupuri-parlamentare-list tbody")
 
         for tr in table_body.select("tr"):
             d = dict()
-            d["url"] = tr.select("td")[1].a.attrs["href"]
-            if tr.select("td")[1].a.string is not None:
-                d["number"] = tr.select("td")[1].a.string.strip()
-            if tr.select("td")[2].string is not None:
-                d["tile"] = tr.select("td")[2].string.strip()
+            d["url"] = urllib.parse.urljoin(url, tr.select("td")[1].a.attrs["href"])
             d["year"] = year
+            d["updated"] = now
+            if tr.select("td")[1].a.string is not None:
+                d["number"] = tr.select("td")[1].a.string.strip().upper()
+            if tr.select("td")[2].text is not None:
+                d["tile"] = tr.select("td")[2].text.strip()
             initiatives.append(d)
+            table.put_item(d)
 
-    return {
-        "initiatives": initiatives,
-        "invoked_function_arn": context.invoked_function_arn
-    }
+    return initiatives
 
 
 if __name__ == "__main__":
-    class Context:
-        def __init__(self):
-            self.function_version = "DemoTest"
-    print(scrape({"years": [2015]}, Context()))
+    for i in scrape({"years": [2017]}, None):
+        print(i)
