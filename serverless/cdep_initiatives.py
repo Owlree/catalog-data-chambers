3f7ecf5ea27c4bd82e7799027dd71fd6972c588f
Robert Badea <owlree128@gmail.com>
2017-11-30 03:53:57 +0200

    Added new code :)

--- serverless/cdep_initiatives.py
+++ serverless/cdep_initiatives.py
@@ -3,6 +3,30 @@ from bs4 import BeautifulSoup
 
 
 def scrape(event: dict, context):
-    response = requests.get("http://www.cdep.ro/pls/proiecte/upl_pck2015.lista?cam=2&anp=2017")
-    bs = BeautifulSoup(response.content)
-    return "Hello, World!"
+
+    urls = []
+    for year in event["years"]:
+        urls.append("http://www.cdep.ro/pls/proiecte/upl_pck2015.lista?cam=2&anp={year}".format(year=year))
+
+    for url in urls:
+        response = requests.get(url)
+        bs = BeautifulSoup(response.content, "lxml")
+        table_body = bs.select_one("#olddiv > div.grup-parlamentar-list.grupuri-parlamentare-list tbody")
+
+        initiatives = []
+
+        for tr in table_body.select("tr"):
+            d = dict()
+            d["url"] = tr.select("td")[1].a.attrs["href"]
+            if tr.select("td")[1].a.string is not None:
+                d["number"] = tr.select("td")[1].a.string.strip()
+            if tr.select("td")[2].string is not None:
+                d["tile"] = tr.select("td")[2].string.strip()
+            initiatives.append(d)
+
+
+    return {"initiatives": initiatives}
+
+
+if __name__ == "__main__":
+    print(scrape({"years": [2015]}, None))
