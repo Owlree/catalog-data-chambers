92af702e99f5f09729c95687f87d17a90c7f09be
Robert Badea <owlree128@gmail.com>
2017-11-30 02:57:40 +0200

    Fixed bs call

--- serverless/cdep_initiatives.py
+++ serverless/cdep_initiatives.py
@@ -4,7 +4,5 @@ from bs4 import BeautifulSoup
 
 def scrape(event: dict, context):
     response = requests.get("http://www.cdep.ro/pls/proiecte/upl_pck2015.lista?cam=2&anp=2017")
-    bs = BeautifulSoup(response)
-    return {
-        "result": "Hello, World!"
-    }
+    bs = BeautifulSoup(response.content)
+    return "Hello, World!"
